a8c2ba0d30133c51d4912aebd73e52daa550a25f
Phestuz <adeshinafestus01@gmail.com>
2022-03-10 12:47:47 -0800

    a C program that prints exactly "Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,37 +1,11 @@
-/*
-
- * File: 4-puts.c
-
- * Auth: Brennan D Baraban <375@holbertonschool.com>
-
- */
-
-
-
-#include <stdio.h>
-
-
-
+#include<stdio.h>
 /**
-
- * main - Prints "Programming is like building a multilingual
-
- *                puzzle, followed by a new line.
-
- *
-
- * Return: Always 0.
-
+ * main - Entry point 
+ * description:  a C program that prints exactly "Programming is like building a multilingual puzzle
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-
-
-	return (0);
-
+        puts("/"Programming is like building a multilingual puzzle");
+        return (0);
 }
