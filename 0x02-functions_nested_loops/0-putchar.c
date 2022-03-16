439a14240fd99d0e88f0d671088656664643dcc3
Phestuz <adeshinafestus01@gmail.com>
2022-03-15 17:39:43 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
-#include "main.h" 
+#include "main.h"
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Succes)
+ * Return: 0
  */
 int main(void)
 {
 	char text[8] = "_putchar";
 	int i;
-
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(text[i]);
