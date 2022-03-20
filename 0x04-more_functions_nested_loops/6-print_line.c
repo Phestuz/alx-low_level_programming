71e6146f144d970b1fadf8bc2e7c29c8e7d983fb
Phestuz <adeshinafestus01@gmail.com>
2022-03-20 01:13:07 -0700

    printing line in chars long

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,25 @@
 #include "main.h"
 
 /**
- * print_line - prints straight line n times.
- * @n: times straight line is printed
- * Return: no return.
+ * print_line - print a line in charl long.
+ * @n: length of line 
+ *
+ * Return: void
  */
 void print_line(int n)
 {
-	int i ;
+	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar(95);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
