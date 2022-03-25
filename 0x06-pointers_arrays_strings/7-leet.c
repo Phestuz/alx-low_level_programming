345da7f6977884085d61f38005cbe64d0b4fe706
Phestuz <adeshinafestus01@gmail.com>
2022-03-24 19:37:19 -0700

    Encoding a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,7 +4,7 @@
  * leet - Encodes a string to 1337
  * @s: The string to be encoded.
  *
- * Return: A pointer to the encoded string
+ * Return: pointer to s
  */
 char *leet(char *s)
 {
@@ -14,12 +14,11 @@ char *leet(char *s)
 
 	stringCount = 0;
 	while (s[stringCount] != '\0')
-		/* check whether leetLetter is found */
 	{
 		leetCount = 0;
 		while (leetCount < 10)
 		{
-			if (leetLetters[leetCount] == str[stringCount])
+			if (leetLetters[leetCount] == s[stringCount])
 			{
 				s[stringCount] = leetNums[leetCount];
 			}
