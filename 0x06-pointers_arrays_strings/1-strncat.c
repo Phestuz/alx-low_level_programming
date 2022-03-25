f0750a54d5f0f719f753fc735854a8b307b596e0
Phestuz <adeshinafestus01@gmail.com>
2022-03-24 18:01:00 -0700

    concatenating two strings using at most an inputed number of bytes from src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,12 @@
+/*
+ * File: 1-strncat.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "main.h"
 
 /**
- * _strncat - function that cooncatenates tw strings. it will use at most n bytes from src, src does not need to be null terminated.
+ * _strncat - function that cooncatenates two strings. it will use at most n bytes from src, src does not need to be null terminated.
  * @dest: pointer to destunation string
  * @src: pointer to source string.
  * @n: number of bytes to be concatenated.
@@ -10,19 +15,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int length, j;
-	/* j is a counter for n bytes of src to be concatenated */
-	/* length = length of destination string */
+	int index = 0, dest_len = 0;
 
-	length = 0;
-	while (dest[length] != '\0')
-	{
-		length++;
-	}
-	for (j = 0; j < n && src[j] != '\0'; j++, length++)
-	{
-		dest[length] = src[j];
-	}
-	dest[length] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
